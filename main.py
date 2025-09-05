6e3a398c7a58988d0df874f37a941c393ed93576
Ali Qazipoor <qazipoor@gmail.com>
2025-09-06 01:34:39 +0300

    Adding play_game() function to initialize the game

--- main.py
+++ main.py
@@ -21,3 +21,44 @@ def calculate_score(cards):
 
     score = sum(cards)
     return score
+
+# Initialize and start the game
+def play_game():
+    print(logo)
+
+    user_cards = []
+    computer_cards = []
+    computer_score = -1
+    user_score = -1
+    is_game_over = False
+
+    # Giving the players first two cards
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    # Asking the user if s/he wants to get a new card or else end the game
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+
+        print(f"Your cards {user_cards}, current score {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    # Giving the computer a new card if the computer's score is not over 17 or Blackjack
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    # Prints the final scores
+    print(f"Your final hand: {user_cards}, final score: {user_score}")
+    print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
