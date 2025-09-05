d3af28466c79edc7f0aa9d05faecfb011f7007e7
Ali Qazipoor <qazipoor@gmail.com>
2025-09-05 15:20:47 +0300

    Adding deal_card() function to return a random card each time it's called

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import random
 from art import logo
+
+# deal_card() function that returns a random card each time it is called
+def deal_card():
+    # Cards deck that contains ace = 1 | 11, 2 to 10 and additional 3 10's for face cards
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
